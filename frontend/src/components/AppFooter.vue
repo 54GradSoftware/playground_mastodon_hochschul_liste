85ff8d27dfd7f7f4ed49308df659a998a507a7c4
Moritz Stückler <moritz@bitbetter.de>
2025-06-30 14:32:15 +0200

    fix: typo in footer

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -7,8 +7,8 @@
       </span>
     </div>
     <div class="col-12 md:col-4">
-      <a href="https://github.com/54GradSoftware/playground_mastodon_hochschul_liste">Github (Software)</a> | <a
-        href="https://github.com/54GradSoftware/playground_mastodon_hochschul_liste/blob/main/LICENSE">MIT Lizens</a>
+      <a href="https://github.com/54GradSoftware/playground_mastodon_hochschul_liste">GitHub (Software)</a> | <a
+        href="https://github.com/54GradSoftware/playground_mastodon_hochschul_liste/blob/main/LICENSE">MIT-Lizenz</a>
     </div>
     <div class="col-12 md:col-4">
       <a href="https://www.54gradsoftware.de/impressum">Impressum</a> | <a
