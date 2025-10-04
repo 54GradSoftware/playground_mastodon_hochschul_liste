9b84a5df874e0f31c0e1f4437de599706054be6f
scammo <sbrinkmann@54gradsoftware.de>
2025-10-04 15:37:55 +0200

    impprove list for bundesbehörden and oberste bundesbehörde

--- backend/src/queries/bundesbehoerden-DE.js
+++ backend/src/queries/bundesbehoerden-DE.js
@@ -7,8 +7,14 @@ SELECT ?item ?itemLabel ?mastodon ?coordinates WHERE {
     ?item wdt:P31/wdt:P279* wd:Q99934885;
       wdt:P17 wd:Q183.
   }
+  UNION
+  {
+    ?item wdt:P31/wdt:P279* wd:Q99935030;
+      wdt:P17 wd:Q183.
+  }  
   ?item wdt:P4033 ?mastodon.
   OPTIONAL { ?item wdt:P625 ?coordinates. }
   SERVICE wikibase:label { bd:serviceParam wikibase:language "de, en". }
-}`
+}
+`
 }
