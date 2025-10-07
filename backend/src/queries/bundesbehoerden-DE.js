2dfcb8588d69773f1b4cc0f8e9903479d5177a72
scammo <sbrinkmann@54gradsoftware.de>
2025-10-07 10:13:26 +0200

    Improve query for bundeshörden

--- backend/src/queries/bundesbehoerden-DE.js
+++ backend/src/queries/bundesbehoerden-DE.js
@@ -11,6 +11,10 @@ SELECT ?item ?itemLabel ?mastodon ?coordinates WHERE {
   {
     ?item wdt:P31/wdt:P279* wd:Q99935030;
       wdt:P17 wd:Q183.
+  } 
+  UNION
+  {
+    ?item wdt:P31/wdt:P279* wd:Q314024;
   }  
   ?item wdt:P4033 ?mastodon.
   OPTIONAL { ?item wdt:P625 ?coordinates. }
