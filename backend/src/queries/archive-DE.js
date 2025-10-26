0c7bc721c7bf60cb4af68d2f1a2a6f5fb5a7babb
scammo <sbrinkmann@54gradsoftware.de>
2025-10-26 18:05:48 +0100

    Improve query archive, fotoarchive, archivverbände

--- backend/src/queries/archive-DE.js
+++ backend/src/queries/archive-DE.js
@@ -7,6 +7,16 @@ export default {
     ?item (wdt:P31/(wdt:P279*)) wd:Q166118;
       wdt:P17 wd:Q183.
   }
+  UNION
+  {
+    ?item (wdt:P31/(wdt:P279*)) wd:Q134401541;
+      wdt:P17 wd:Q183.
+  }
+  UNION
+  {
+    ?item (wdt:P31/(wdt:P279*)) wd:Q27032363;
+      wdt:P17 wd:Q183.
+  }
   ?item wdt:P4033 ?mastodon.
   OPTIONAL { ?item wdt:P625 ?coordinates. }
   SERVICE wikibase:label { bd:serviceParam wikibase:language "de, en". }
